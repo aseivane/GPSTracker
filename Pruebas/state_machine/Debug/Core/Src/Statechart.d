389afc2d6471e084e8ac94e56a76ddd7fe8b33f9
Andres Seivane <=>
2022-06-26 20:28:37 -0300

    prueba state chart

---
+++ Pruebas/state_machine/Debug/Core/Src/Statechart.d
@@ -0,0 +1,9 @@
+Core/Src/Statechart.o: ../Core/Src/Statechart.c \
+ ../Core/Src/../Inc/sc_types.h ../Core/Src/../Inc/Statechart.h \
+ ../Core/Src/../Inc/sc_types.h ../Core/Src/../Inc/Statechart_required.h \
+ ../Core/Src/../Inc/Statechart.h
+../Core/Src/../Inc/sc_types.h:
+../Core/Src/../Inc/Statechart.h:
+../Core/Src/../Inc/sc_types.h:
+../Core/Src/../Inc/Statechart_required.h:
+../Core/Src/../Inc/Statechart.h:
